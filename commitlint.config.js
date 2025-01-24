197bd1be014abccb74ac931f2acdbd3b31f97aed
Yuri Cavalini <yuricavalini@gmail.com>
2025-01-24 08:58:40 -0300

    chore(courses-state-management): add commitlint rule for scope-enum

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-empty': [2, 'never'], // Enforces the presence of a scope
+    'scope-empty': [2, 'never'], // Enforces the presence of a scope,
+    'scope-enum': [2, 'always', ['courses-state-management']], // Only allows 'courses-state-management' as scope
   },
 };
