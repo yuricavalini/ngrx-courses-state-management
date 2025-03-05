22bf270ef5a2b854efcee486c5df603840d6bbd1
Yuri Cavalini <yuricavalini@gmail.com>
2025-03-05 08:43:53 -0300

    refactor(courses-state-management): move conditionally dispatched action logic to effect

--- src/app/courses/courses.resolver.ts
+++ src/app/courses/courses.resolver.ts
@@ -5,28 +5,14 @@ import {
   RouterStateSnapshot,
 } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { filter, finalize, first, tap } from 'rxjs/operators';
 
-import { loadAllCourses } from './course.actions';
-import { selectAreCoursesLoaded } from './courses.selectors';
+import { coursesPageActions } from './action-types';
 
 @Injectable()
 export class CoursesResolver implements Resolve<any> {
-  private loading = false;
-
   constructor(private store: Store) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.store.select(selectAreCoursesLoaded).pipe(
-      tap(coursesLoaded => {
-        if (!this.loading && !coursesLoaded) {
-          this.loading = true;
-          this.store.dispatch(loadAllCourses());
-        }
-      }),
-      filter(coursesLoaded => coursesLoaded),
-      first(),
-      finalize(() => (this.loading = false))
-    );
+    return this.store.dispatch(coursesPageActions.openedCoursesList());
   }
 }
